7894b42c32ec533f9f26772421eef19c0462b5b4
Dmitry <bd.morozov@gmail.com>
2021-06-05 19:07:34 +0300

    add episodes

--- podcasts.php
+++ podcasts.php
@@ -2,16 +2,137 @@
         <?php $page = 'podcasts'; include('menu.php') ?>
         <div class="content _hide">
             <section class="podcasts">
-            <!-- <iframe frameborder="0" style="border:none;width:100%;height:180px;" width="100%" height="180" src="https://music.yandex.ru/iframe/#track/74845786/13114271">Слушайте <a href='https://music.yandex.ru/album/13114271/track/74845786'>Лига Подкастеров - Второй пилот</a> на Яндекс.Музыке</iframe> -->
-            <div class="player">
-                <div class="player__wrapper">
-                    <div class="player__buttons">
-                        <button class="player__button _icon-backward"></button>
-                        <button class="player__button _icon-play"></button>
-                        <button class="player__button _icon-forward"></button>
+                <div class="podcasts__episodes">
+                    
+                    <div class="podcasts__episode episode">
+                        <div class="episode__cover">
+                            <img src="/img/covers/cover_1.jpg" alt="">
+                        </div>
+                        <div class="episode__text">
+                            <div class="episode__title">
+                                <h3>Выпуск 5: Гостевой</h3>
+                            </div>
+                            <div class="episode__description">
+                                <p>Казалось бы, Муж в гостях у Лиги, но на деле Лига в гостях у Мужа. Идеальный Муж не только почесал с нами языками, но и обучил своему кулинарному Дзюцу!
+                                    <br> В юбилейном пятом выпуске даже усечённый по самое нЕльзя состав не помешал нам узнать:
+                                        <br>- душещипательную и витиеватую историю жизни Идеального Мужа;
+                                        <br>- подробности закулисной жизни пауэрлифтеров;
+                                        <br>- можно ли совмещать спорт и киберспорт;
+                                        <br>- секрет и д е а л ь н о г о отпуска;
+                                        <br>- покорит ли наш гость TikTok.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="episode__links links">
+                            <a href="" class="episode__link _icon-vk"></a>
+                            <a href="" class="episode__link _icon-spotify"></a>
+                            <a href="" class="episode__link _icon-sloud"></a>
+                        </div>
+                    </div>
+                    <div class="podcasts__episode episode">
+                        <div class="episode__cover">
+                            <img src="/img/covers/cover_2.jpg" alt="">
+                        </div>
+                        <div class="episode__text">
+                            <div class="episode__title">
+                                <h3>Выпуск 4</h3>
+                            </div>
+                            <div class="episode__description">
+                                <p>Несколько фактов о числе "четыре" (4 (IV (100₂ (четвертушка)))):
+                                    <br>- это натуральное число, расположенное между 3 и 5;
+                                    <br>- именно столько метров имеет в диаметре труба, в которой мы записали этот выпуск подкаста;
+                                    <br>- столько участников было в первом пилоте.
+                                    <br>В IV выпуске мы решили обсудить татуировки с точки зрения вкусовых предпочтений, вспомнили о важности чистки зубов и обратились к вопросам финансовой грамотности.</p>
+                            </div>
+                        </div>
+                        <div class="episode__links links">
+                            <a href="" class="episode__link _icon-vk"></a>
+                            <a href="" class="episode__link _icon-spotify"></a>
+                            <a href="" class="episode__link _icon-sloud"></a>
+                        </div>
+                    </div>
+                    <div class="podcasts__episode episode">
+                        <div class="episode__cover">
+                            <img src="/img/covers/cover_3.jpg" alt="">
+                        </div>
+                        <div class="episode__text">
+                            <div class="episode__title">
+                                <h3>Выпуск 3</h3>
+                            </div>
+                            <div class="episode__description">
+                                <p>В этом выпуске мы изо всех сил постараемся не прослыть расистами, разберёмся, какому когнитивному искажению подарила своё имя IKEA и как с этим связана программа "Играй, гармонь!", разделим ностальгию Лёхи по детству в деревне и услышим самую необычную аннотацию к "Острым козырькам", прочитав которую, Киллиан Мёрфи отказался от ролей у Нолана из-за сценарной скупости.</p>
+                            </div>
+                        </div>
+                        <div class="episode__links links">
+                            <a href="" class="episode__link _icon-vk"></a>
+                            <a href="" class="episode__link _icon-spotify"></a>
+                            <a href="" class="episode__link _icon-sloud"></a>
+                        </div>
+                    </div>
+                    <div class="podcasts__episode episode">
+                        <div class="episode__cover">
+                            <img src="/img/covers/cover_4.jpg" alt="">
+                        </div>
+                        <div class="episode__text">
+                            <div class="episode__title">
+                                <h3>Выпуск 2</h3>
+                            </div>
+                            <div class="episode__description">
+                                <p>Во втором пилоте нашего концептуального подкаста мы обсудили дистанционное обучение, закостенелость старого поколения и бальзамический уксус.
+                                    <br>Ставь лайк, если масло черного тмина – западная рэпчина.
+                                    <br>Ставь лайк, если одна буханка хлеба стоит 60 рублей по Лехиному курсу.</p>
+                            </div>
+                        </div>
+                        <div class="episode__links links">
+                            <a href="" class="episode__link _icon-vk"></a>
+                            <a href="" class="episode__link _icon-spotify"></a>
+                            <a href="" class="episode__link _icon-sloud"></a>
+                        </div>
+                    </div>
+                    <div class="podcasts__episode episode">
+                        <div class="episode__cover">
+                            <img src="/img/covers/cover_5.jpg" alt="">
+                        </div>
+                        <div class="episode__text">
+                            <div class="episode__title">
+                                <h3>Выпуск 1: Пилот</h3>
+                            </div>
+                            <div class="episode__description">
+                                <p>
+                                    В пилотном выпуске мы сразу же очертили список своих врагов, нашли секрет победы на выборах в президенты США, обсосали "Метод" Юрия Быкова и безуспешно зовём #пиврумтула стать нашим спонсором.
+                                    <br>
+                                    <br>И да, гастрошумы это не баг, это фича.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="episode__links links">
+                            <a href="" class="episode__link _icon-vk"></a>
+                            <a href="" class="episode__link _icon-spotify"></a>
+                            <a href="" class="episode__link _icon-sloud"></a>
+                        </div>
+                    </div>
+                </div> 
+                <div class="podcasts_player player">
+                    <div class="player__wrapper">
+                        <div class="player__controls">
+                            <button class="player__button _icon-backward"></button>
+                            <button class="player__button _icon-play"></button>
+                            <button class="player__button _icon-forward"></button>
+                        </div>
+                        <div class="player__cover"></div>
+                        <div class="player__text">
+                            <div class="player__title">Выпуск 1</div>
+                        </div>
+                        <audio src="" class="player__audio"></audio>
+                        <div class="player__progress-bar">
+                            <div class="player__progress"></div>
+                        </div>
+                        <div class="player__settings">
+                            <!-- <button class="player__button _icon-like"></button> -->
+                            <button class="player__button _icon-volume-low"></button>
+                        </div>
                     </div>
                 </div>
-            </div>
             </section>
         </div>
     </div>
